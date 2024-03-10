72b1c7c0c2410eafb2c6b2fd521dabeb87e9dec7
explorer3039 <2124986348@qq.com>
2024-03-10 22:46:45 +0800

    1.17 deepslate gen

--- src/main/java/cn/nukkit/level/generator/Normal.java
+++ src/main/java/cn/nukkit/level/generator/Normal.java
@@ -50,7 +50,7 @@ public class Normal extends Generator {
     }
 
     private List<Populator> generationPopulators = ImmutableList.of(
-        new PopulatorDeepslate(),
+        //new PopulatorDeepslate(),
         new PopulatorBedrock(),
         new PopulatorGroundCover()
     );
@@ -67,17 +67,18 @@ public class Normal extends Generator {
             new OreType(Block.get(BlockID.GRAVEL), 8, 33, 0, 128),
             new OreType(Block.get(BlockID.STONE, BlockStone.GRANITE), 10, 33, 0, 80),
             new OreType(Block.get(BlockID.STONE, BlockStone.DIORITE), 10, 33, 0, 80),
-            new OreType(Block.get(BlockID.STONE, BlockStone.ANDESITE), 10, 33, 0, 80)
+            new OreType(Block.get(BlockID.STONE, BlockStone.ANDESITE), 10, 33, 0, 80),
+            new OreType(Block.get(BlockID.DEEPSLATE), 12, 33, 0, 16)
         }),
         new PopulatorOre(BlockID.DEEPSLATE, new OreType[]{
-                        new OreType(Block.get(BlockID.DEEPSLATE_COAL_ORE), 20, 17, 0, 8),
-                        new OreType(Block.get(BlockID.DEEPSLATE_COPPER_ORE), 20, 9, 0, 8),
-                        new OreType(Block.get(BlockID.DEEPSLATE_IRON_ORE), 20, 9, 0, 8),
-                        new OreType(Block.get(BlockID.DEEPSLATE_REDSTONE_ORE), 8, 8, 0, 8),
-                        new OreType(Block.get(BlockID.DEEPSLATE_LAPIS_ORE), 1, 7, 0, 8),
-                        new OreType(Block.get(BlockID.DEEPSLATE_GOLD_ORE), 2, 9, 0, 8),
-                        new OreType(Block.get(BlockID.DEEPSLATE_DIAMOND_ORE), 1, 8, 0, 8),
-                        new OreType(Block.get(BlockID.TUFF), 20, 17, 0, 8)
+                        new OreType(Block.get(BlockID.DEEPSLATE_COAL_ORE), 20, 17, 0, 16),
+                        new OreType(Block.get(BlockID.DEEPSLATE_COPPER_ORE), 20, 9, 0, 16),
+                        new OreType(Block.get(BlockID.DEEPSLATE_IRON_ORE), 20, 9, 0, 16),
+                        new OreType(Block.get(BlockID.DEEPSLATE_REDSTONE_ORE), 8, 8, 0, 16),
+                        new OreType(Block.get(BlockID.DEEPSLATE_LAPIS_ORE), 1, 7, 0, 16),
+                        new OreType(Block.get(BlockID.DEEPSLATE_GOLD_ORE), 2, 9, 0, 16),
+                        new OreType(Block.get(BlockID.DEEPSLATE_DIAMOND_ORE), 1, 8, 0, 16),
+                        new OreType(Block.get(BlockID.TUFF), 10, 33, 0, 16)
         }),
         new PopulatorCaves()
     );
