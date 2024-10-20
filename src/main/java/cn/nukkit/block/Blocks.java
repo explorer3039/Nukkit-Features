259d0cd0488b3456a5b11ba332fd37d3307d9d78
explorer3039 <2124986348@qq.com>
2024-10-20 12:42:46 +0800

    thank you git

--- src/main/java/cn/nukkit/block/Blocks.java
+++ src/main/java/cn/nukkit/block/Blocks.java
@@ -346,10 +346,8 @@ public class Blocks {
         list[FLETCHING_TABLE] = BlockFletchingTable.class; //456
         list[SMITHING_TABLE] = BlockSmithingTable.class; //457
         list[BARREL] = BlockBarrel.class; //458
-<<<<<<< HEAD
-=======
+
         list[LOOM] = BlockLoom.class; //459
->>>>>>> upstream/master
 
         list[BELL] = BlockBell.class; //461
         list[SWEET_BERRY_BUSH] = BlockSweetBerryBush.class; //462
